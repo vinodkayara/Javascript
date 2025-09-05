fb5aaffc309beacd098094c7ba1ae11cfc19fc2f
Vinod Kayara <115912502+vinodkayara@users.noreply.github.com>
2025-09-05 22:11:10 +0530

    Add files via upload

---
+++ ArrayDemo.js
@@ -0,0 +1,20 @@
+//using literals 
+let vin=['vinod',22,true,{city:"bengalur",state:"Karnataka"},45.55]
+
+let vik=new Array('vinod',22,true,{city:"bengalur",state:"Karnataka"},45.55)
+
+console.log(vin)
+console.log(vik)
+
+//how to access the elemnets present inside the array
+console.log(vik[1])
+
+//how to find the length of given array
+console.log(vik.length)
+
+//2D array
+let arr=['virat',18,true,[10,'anushka',false],{name:'ral',city:'manglore'},false,8589.54]
+console.log(arr)
+
+console.log(arr[3][1])
+console.log(arr[4].city)
---
+++ ArrayMethods.js
@@ -0,0 +1,13 @@
+let vin=['vinod',22,true,{city:"bengalur",state:"Karnataka"},45.55]
+console.log(vin)
+
+//push()
+vin.push('vikas')
+vin.push(true)
+vin.push(18)
+console.log(vin)
+
+
+//pop()
+vin.pop()
+console.log(vin)
