beeb107b1178cc64d5fa44478e794545aa6946a6
Vinod Kayara <115912502+vinodkayara@users.noreply.github.com>
2025-09-08 21:52:26 +0530

    Add files via upload

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -11,3 +11,40 @@ console.log(vin)
 //pop()
 vin.pop()
 console.log(vin)
+
+//unShift()
+vin.unshift(1)
+console.log(vin)
+
+//shift()
+vin.shift()
+console.log(vin)
+
+
+//IndexOf()
+console.log(vin.indexOf(22))
+console.log(vin.indexOf(45))
+
+//lastIndesx()
+console.log(vin.lastIndexOf(true))
+
+//join()
+console.log(vin.join())
+
+//includes()
+console.log(vin.includes('vinod'))
+console.log(vin.includes('hello'))
+
+//reverse()
+console.log(vin.reverse())
+
+//slice()
+console.log(vin.slice(0,5)) //it will not alter original array
+console.log(vin)
+
+//splice()
+let vik=[1,2,3,4,5,6,8,9]
+console.log(vik)
+console.log(vik.splice(2,4))
+console.log(vik)  //it will altetr the original array
+
